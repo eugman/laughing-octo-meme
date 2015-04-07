e6d2c7aa3961f7d2cac6beac13115e45f12b9c9d
Eugene Meidinger <eugene@devlaptop.localdomain>
2015-04-07 18:05:34 -0400

    Changed to be run as root

--- fedora.sh
+++ fedora.sh
@@ -1,41 +1,41 @@
 #update everything
-su -c "yum -y update"
+yum -y update
 
 #install command line tools
-su -c "yum -y install htop lynx irssi screen tmux mc nmap wget"
+yum -y install htop lynx irssi screen tmux mc nmap wget
 
 #install command line coding tools
-su -c "yum -y install git valgrind"
+yum -y install git valgrind
 
 #install samba
-su -c "yum -y install samba samba-client system-config-samba cifs-utils"
+yum -y install samba samba-client system-config-samba cifs-utils
 
 #install c tools
-su -c "yum -y groupinstall development-tools"
-su -c "yum -y install libedit-dev* man-pages libstdc++-docs"
+yum -y groupinstall development-tools
+yum -y install libedit-dev* man-pages libstdc++-docs
 
 #install C# programming
-su -c "yum -y install monodevelop monodevelop-versioncontrol monodevelop-database"
+yum -y install monodevelop monodevelop-versioncontrol monodevelop-database
 
 #install haskell
-su -c "yum -y install ghc cabal-install"
+yum -y install ghc cabal-install
 cabal update
 cabal install vector statistics attoparsec
 
 #install ruby
-su -c "yum -y install ruby"
+yum -y install ruby
 gem install sinatra haml rails slim shotgun
 
 #install lamp
-su -c "yum -y install httpd mysql mysql-server php php-mysql php-myadmin"
-su -c "service httpd start; service mysql start"
-#su -c "/user/bin/mysql_secure_installation"
+yum -y install httpd mysql mysql-server php php-mysql php-myadmin
+service httpd start; service mysql start
+#/user/bin/mysql_secure_installation
 
 #install mean
 #http://docs.mongodb.org/manual/tutorial/install-mongodb-on-red-hat/
-su -c "yum -y install mongodb-org nodejs npm"
-su -c "npm -g install express bower"
-su -c "service mongod start"
+yum -y install mongodb-org nodejs npm
+npm -g install express bower
+service mongod start
 
 #install desktop apps
-su -c "yum -y install eric geany lazarus drpython remmina remmina-plugin-rdp zenmap"
+yum -y install eric geany lazarus drpython remmina remmina-plugin-rdp zenmap
