1fcbadd0425c055901b29cc1d1943f20be934f2f
Eugene Meidinger <eugene@eugenemeidinger.com>
2015-04-07 12:56:19 -0400

    Added some repos

--- eveyone.sh
+++ eveyone.sh
@@ -1,2 +1,8 @@
 cp .vimrc ~/.vimrc
 cp .tmux.conf ~/.tmux.conf
+
+mkdir  progProj
+cd progProj
+git clone https://github.com/fuzyll/coppermind.git
+git clone https://github.com/eugman/learnCTheHardWay.git
+git clone https://github.com/eugman/buildYourOwnLisp.git
