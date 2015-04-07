6ebe01724693ad4256729ae5f853c873943d2f66
Eugene Meidinger <eugene@devlaptop.localdomain>
2015-04-07 18:10:56 -0400

    Removed sudo. Added git config

--- eveyone.sh
+++ eveyone.sh
@@ -1,8 +1,15 @@
+#Copy dotfiles
 cp .vimrc ~/.vimrc
 cp .tmux.conf ~/.tmux.conf
 
-mkdir  progProj
-cd progProj
+#configure git
+git config --global color.ui true
+git config --global user.name "Eugene Meidinger"
+git config --global user.email eugene@eugenemeidinger.com
+#git config --global push.default simple
+
+mkdir  ~/progProj
+cd ~/progProj
 git clone https://github.com/fuzyll/coppermind.git
 git clone https://github.com/eugman/learnCTheHardWay.git
 git clone https://github.com/eugman/buildYourOwnLisp.git
--- rasp.sh
+++ rasp.sh
@@ -1,37 +1,37 @@
 #!/user/bin/env bash
 
 #Update everything
-sudo apt-get update && sudo apt-get -y upgrade
+apt-get update && apt-get -y upgrade
 
 #Install command line tools
-sudo apt-get -y install htop lynx irssi screen tmux wicd-config aptitude mc nmap wget
+apt-get -y install htop lynx irssi screen tmux wicd-config aptitude mc nmap wget
 
 #install samaba
-sudo apt-get install cifs-utils samaba-common-bin
+apt-get install cifs-utils samaba-common-bin
 
 #Install command line coding tools
-sudo apt-get -y install git valgrind
+apt-get -y install git valgrind
 
 #install c programming
-sudo apt-get -y install build-essential libedit-dev manpages-dev
+apt-get -y install build-essential libedit-dev manpages-dev
 
 #install C# programming
-sudo apt-get -y install mono-complete
+apt-get -y install mono-complete
 
 #install haskell
-sudo apt-get -y install ghc cabal-install
+apt-get -y install ghc cabal-install
 cabal update
 cabal install vector statistics attoparsec
 
 #install ruby
-sudo apt-get install ruby
+apt-get install ruby
 gem install sinatra haml slim rails shotgun
 
 #install lamp
-sudo apt-get install apache2 php5 mysql-client mysql-server php-myadmin
+apt-get install apache2 php5 mysql-client mysql-server php-myadmin
 
 #install desktop apps
-sudo apt-get -y install iceweasel geany remmina remmina-plugin-rdp evince zenmap
+apt-get -y install iceweasel geany remmina remmina-plugin-rdp evince zenmap
 
 
 
