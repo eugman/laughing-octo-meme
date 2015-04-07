bba51524c75fbdc2ff134f46c7b727e725f0f317
Eugene Meidinger <eugene@eugenemeidinger.com>
2015-04-07 12:28:38 -0400

    Organized the sections a bit

--- windows.ps1
+++ windows.ps1
@@ -2,27 +2,41 @@
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
-choco install  flahplayerplugin
+
+#all computers
+choco install notepadplusplus.install
+choco install 7zip.install
+choco install dropbox
+choco install treesizefree
+
+#personal computers
+choco install flashplayerplugin
+choco install steam
+choco isntall skype
+choco install itunes
+choco install f.lux
+choco install qbittorrent 
+
+
+#development computers
 choco install git.install
 choco install googlechrome
 choco install javaruntime
-choco install notepadplusplus.install
-choco install 7zip.install
 choco install atom
 choco install powershell
 choco isntall nodejs.install
 choco install chocolateygui
 choco install ruby
 choco isntall filezilla
-choco isntall skype
 choco isntall putty
 choco install python
 choco isntall vim
-choco install dropbox
 choco isntall virtualbox
 choco install fiddler
 choco install wireshark
 choco install winmerge
-choco install itunes
 choco install webpi
 choco install nmap
+choco install dotpeek 
+choco install mongodb
+choco install ilspy
