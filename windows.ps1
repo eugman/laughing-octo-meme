cc8cd119b1fda130132198cd8fec1106b1836461
Eugene Meidinger <eugene@eugenemeidinger.com>
2015-04-07 08:50:28 -0400

    Started powershell script

---
+++ windows.ps1
@@ -0,0 +1,28 @@
+#NOT TESTED YET
+iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+
+choco install  flahplayerplugin
+choco install git.install
+choco install googlechrome
+choco install javaruntime
+choco install notepadplusplus.install
+choco install 7zip.install
+choco install atom
+choco install powershell
+choco isntall nodejs.install
+choco install chocolateygui
+choco install ruby
+choco isntall filezilla
+choco isntall skype
+choco isntall putty
+choco install python
+choco isntall vim
+choco install dropbox
+choco isntall virtualbox
+choco install fiddler
+choco install wireshark
+choco install winmerge
+choco install itunes
+choco install webpi
+choco install nmap
