89b1987a166082e455306277da081663179cf73f
Eugene Meidinger <eugene@eugenemeidinger.com>
2015-04-09 16:51:58 -0400

    added check for root

--- fedora.sh
+++ fedora.sh
@@ -1,43 +1,51 @@
-#update everything
-yum -y update
+if [ $EUID -z ]
+then
+	echo -e '\e[1;31 Run as bash! \e[0m'
+elif [[ $EUID -ne 0]]
+then
+	echo -e "\e[1;31 You should be root! \e[0m"
+else
+	#update everything
+	yum -y update
 
-#install command line tools
-yum -y install htop lynx irssi screen tmux mc nmap wget
+	#install command line tools
+	yum -y install htop lynx irssi screen tmux mc nmap wget
 
-#install command line coding tools
-yum -y install git valgrind
+	#install command line coding tools
+	yum -y install git valgrind
 
-#install samba
-yum -y install samba samba-client system-config-samba cifs-utils
+	#install samba
+	yum -y install samba samba-client system-config-samba cifs-utils
 
-#install c tools
-yum -y groupinstall development-tools
-yum -y install libedit-dev* man-pages libstdc++-docs
+	#install c tools
+	yum -y groupinstall development-tools
+	yum -y install libedit-dev* man-pages libstdc++-docs
 
-#install C# programming
-yum -y install monodevelop monodevelop-versioncontrol monodevelop-database
+	#install C# programming
+	yum -y install monodevelop monodevelop-versioncontrol monodevelop-database
 
-#install haskell
-yum -y install ghc cabal-install
-cabal update
-cabal install vector statistics attoparsec criterion
+	#install haskell
+	yum -y install ghc cabal-install
+	cabal update
+	cabal install vector statistics attoparsec criterion
 
-#install ruby
-yum -y install ruby
-gem install sinatra haml rails slim shotgun pry
-#add gems for fuzzyl's stuff
-gem install json git redcarpet
+	#install ruby
+	yum -y install ruby
+	gem install sinatra haml rails slim shotgun pry
+	#add gems for fuzzyl's stuff
+	gem install json git redcarpet
 
-#install lamp
-yum -y install httpd mysql mysql-server php php-mysql php-myadmin
-service httpd start; service mysql start
-#/user/bin/mysql_secure_installation
+	#install lamp
+	yum -y install httpd mysql mysql-server php php-mysql php-myadmin
+	service httpd start; service mysql start
+	#/user/bin/mysql_secure_installation
 
-#install mean
-#http://docs.mongodb.org/manual/tutorial/install-mongodb-on-red-hat/
-yum -y install mongodb-org nodejs npm
-npm -g install express bower
-service mongod start
+	#install mean
+	#http://docs.mongodb.org/manual/tutorial/install-mongodb-on-red-hat/
+	yum -y install mongodb-org nodejs npm
+	npm -g install express bower
+	service mongod start
 
-#install desktop apps
-yum -y install eric geany lazarus drpython remmina remmina-plugin-rdp zenmap
+	#install desktop apps
+	yum -y install eric geany lazarus drpython remmina remmina-plugin-rdp zenmap
+fi
--- rasp.sh
+++ rasp.sh
@@ -1,40 +1,44 @@
 #!/user/bin/env bash
 
-#Update everything
-apt-get update && apt-get -y upgrade
+if [ $UID -ne 0]; then
+	echo Non root user!
+else
+	
+	#Update everything
+	apt-get update && apt-get -y upgrade
 
-#Install command line tools
-apt-get -y install htop lynx irssi screen tmux wicd-config aptitude mc nmap wget ssh ssh-server
+	#Install command line tools
+	apt-get -y install htop lynx irssi screen tmux wicd-config aptitude mc nmap wget ssh ssh-server
 
-#install samaba
-apt-get install cifs-utils samaba-common-bin
+	#install samaba
+	apt-get install cifs-utils samaba-common-bin
 
-#Install command line coding tools
-apt-get -y install git valgrind python-rpi.gpio
+	#Install command line coding tools
+	apt-get -y install git valgrind python-rpi.gpio
 
-#install c programming
-apt-get -y install build-essential libedit-dev manpages-dev
+	#install c programming
+	apt-get -y install build-essential libedit-dev manpages-dev
 
-#install C# programming
-apt-get -y install mono-complete
+	#install C# programming
+	apt-get -y install mono-complete
 
-#install haskell
-apt-get -y install ghc cabal-install
-cabal update
-cabal install vector statistics attoparsec criterion
+	#install haskell
+	apt-get -y install ghc cabal-install
+	cabal update
+	cabal install vector statistics attoparsec criterion
 
-#install ruby
-apt-get install ruby
-gem install sinatra haml slim rails shotgun pry
-#install gems for fuzzyl's stuff
-gem install git json redcarpet
+	#install ruby
+	apt-get install ruby
+	gem install sinatra haml slim rails shotgun pry
+	#install gems for fuzzyl's stuff
+	gem install git json redcarpet
 
-#install lamp
-apt-get install apache2 php5 mysql-client mysql-server phpmyadmin
+	#install lamp
+	apt-get install apache2 php5 mysql-client mysql-server phpmyadmin
 
-#install desktop apps
-apt-get -y install iceweasel geany remmina remmina-plugin-rdp evince zenmap
+	#install desktop apps
+	apt-get -y install iceweasel geany remmina remmina-plugin-rdp evince zenmap
 
-
+fi
 
 #todo: append iceweasel to autostart
