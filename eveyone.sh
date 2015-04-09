f4a45afd9bf25d9167e6962c10c974f17e333220
Eugene Meidinger <eugene@eugenemeidinger.com>
2015-04-08 20:58:44 -0400

    Added vim as default git editor

--- eveyone.sh
+++ eveyone.sh
@@ -3,6 +3,7 @@ cp .vimrc ~/.vimrc
 cp .tmux.conf ~/.tmux.conf
 
 #configure git
+git config --global core.editor "vim"
 git config --global color.ui true
 git config --global user.name "Eugene Meidinger"
 git config --global user.email eugene@eugenemeidinger.com
