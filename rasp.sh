128b418da5123c07ccb87ca906ecf90adc947362
Eugene Meidinger <eugene@eugenemeidinger.com>
2015-04-09 20:25:29 -0400

    added more cleans

--- rasp.sh
+++ rasp.sh
@@ -11,16 +11,19 @@ else
 	apt-get -y install htop lynx irssi screen tmux wicd-config aptitude mc nmap wget ssh ssh-server
 
 	#Install command line games
-	apt-get install zangband ADOM nethack bombardier tome
+	apt-get -y install zangband ADOM nethack bombardier tome
 
 	#install samaba
-	apt-get install cifs-utils samaba-common-bin
+	apt-get -y install cifs-utils samaba-common-bin
 
 	#Install command line coding tools
 	apt-get -y install git valgrind python-rpi.gpio
 
 	#install c programming
 	apt-get -y install build-essential libedit-dev manpages-dev
+	
+	#clean again
+	apt-get clean
 
 	#install C# programming
 	apt-get -y install mono-complete monodevelop
@@ -44,6 +47,9 @@ else
 	sudo dpkg -i node_latest_armhf.deb
 	rm node_latest_armhf.deb
 	npm -g install azure-cli express bower
+
+	#clean again, just to be safe
+	apt-get clean
 
 	#install desktop apps
 	apt-get -y install iceweasel geany remmina remmina-plugin-rdp evince zenmap
