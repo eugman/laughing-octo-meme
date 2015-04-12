5818dbadbf309865848e99223fc1f016d9b79710
Eugene Meidinger <eugene@eugenemeidinger.com>
2015-04-12 15:53:38 -0400

    Temp disable advanced fedora features

--- master.sh
+++ master.sh
@@ -153,17 +153,17 @@ then
 		#install a bunch of random stuff, I don't know if any of this works
 		yum -y install scala erlang eclipse golang rust clojure dmd ldc gdc
 
-		if [[ $pi == 'y' ]] ||   yum -q list steam | grep -q steam
-		then
-			#???
-		else
-			yum -y install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-stable.noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-stable.noarch.rpm
+#		if [[ $pi == 'y' ]] ||   yum -q list steam | grep -q steam
+#		then
+#			#???
+#		else
+#			yum -y install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-stable.noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-stable.noarch.rpm
 		
-			#install codecs 
-			yum -y install gstreamer{1,}-{ffmpeg,libav,plugins-{good,ugly,bad{,-free,-nonfree}}} ffmpeg 
-			yum -y install steam
-			yum install gnash-plugin
-		fi
+#			#install codecs 
+#			yum -y install gstreamer{1,}-{ffmpeg,libav,plugins-{good,ugly,bad{,-free,-nonfree}}} ffmpeg 
+#			yum -y install steam
+#			yum install gnash-plugin
+#		fi
 	fi
 fi
 
