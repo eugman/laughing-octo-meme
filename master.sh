ff313e778a2d797063104f79991de9cc11a705be
Eugene Meidinger <eugene@eugenemeidinger.com>
2015-04-12 16:06:10 -0400

    Added gparted

--- master.sh
+++ master.sh
@@ -94,7 +94,7 @@ then
 	if [[ $desktop == 'y' ]]
 	then
 		#install desktop apps
-		apt-get -y install iceweasel geany remmina remmina-plugin-rdp evince zenmap
+		apt-get -y install iceweasel geany remmina remmina-plugin-rdp evince zenmap gparted
 #		if [[ -e /etc/xdg/lxsession/LXDE-pi/autostart && ! grep -q iceweasel /etc/xdg/lxsession/LXDE-pi/autostart ]]	
 #		then
 #			echo "@iceweasel" >> /etc/xdg/lxsession/LXDE-pi/autostart
@@ -147,7 +147,7 @@ then
 	if [[ $desktop = 'y' ]]
 	then
 		#install desktop apps
-		yum -y install eric geany lazarus drpython remmina remmina-plugin-rdp zenmap
+		yum -y install eric geany lazarus drpython remmina remmina-plugin-rdp zenmap gparted
 		
 
 		#install a bunch of random stuff, I don't know if any of this works
