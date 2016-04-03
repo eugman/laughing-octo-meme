2858dad334257750285a3a8c131b0c7b014591db
Eugene Meidinger <eugene@eugenemeidinger.com>
2016-04-02 21:36:58 -0400

    Fixed the windows script

--- windows.ps1
+++ windows.ps1
@@ -1,46 +1,48 @@
+# Allow scripts to run
+#Set-ExecutionPolicy RemoteSigned
+
 #NOT TESTED YET
-iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
-SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+#iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+
 
 
 #all computers
-choco install notepadplusplus.install
-choco install 7zip.install
-choco install dropbox
-choco install treesizefree
+choco install notepadplusplus.install -y
+choco install 7zip.install -y
+choco install dropbox -y
+choco install treesizefree -y
 
 #personal computers
-choco install flashplayerplugin
-choco install steam
-choco isntall skype
-choco install itunes
-choco install f.lux
-choco install qbittorrent 
+choco install flashplayerplugin -y
+choco install steam -y
+choco install skype -y
+choco install itunes -y
+choco install f.lux -y
+choco install qbittorrent -y
+choco install vmware-horizon-client -y  
 
 
 #development computers
-choco install git.install
-choco install googlechrome
-choco install javaruntime
-choco install atom
-choco install powershell
-choco isntall nodejs.install
-choco install chocolateygui
-choco install ruby
-choco isntall filezilla
-choco isntall putty
-choco install python
-choco isntall vim
-choco isntall virtualbox
-choco install fiddler
-choco install wireshark
-choco install winmerge
-choco install webpi
-choco install nmap
-choco install dotpeek 
-choco install mongodb
-choco install ilspy
+choco install git.install -y
+choco install googlechrome -y
+choco install javaruntime -y
+choco install visualstudiocode -y
+choco install powershell -y
+choco install nodejs.install -y
+choco install chocolateygui -y
+choco install ruby -y
+choco install filezilla -y
+choco install putty -y
+choco install python -y
+choco install virtualbox -y
+choco install fiddler -y
+choco install wireshark -y
+choco install winmerge -y
+choco install webpi -y
+choco install nmap -y
+choco install dotpeek -y
+choco install ilspy -y
 
 
 #servers
-Install-WindowsFeature -name hyper-v -IncludeManagementTools #-Restart
+#Install-WindowsFeature -name hyper-v -IncludeManagementTools #-Restart
