64064177adfdbc438f17ec93a6220a2c90f6c9e2
Eugene Meidinger <eugene@eugenemeidinger.com>
2016-04-13 21:42:20 -0400

    Updated server script. Generalized changes a bit.

--- windows_server/Configure Server.ps1
+++ windows_server/Configure Server.ps1
@@ -2,12 +2,12 @@
 
 
 #http://www.adminarsenal.com/admin-arsenal-blog/using-powershell-to-set-static-and-dhcp-ip-addresses-part-1/
-$IP = "192.168.0.200"
+$IP = "192.168.0.244"
 $MaskBits = 24 # This means subnet mask = 255.255.255.0
 $Gateway = "192.168.0.1"
-#$Dns = "192.168.0.200"
+$Dns = "192.168.0.200"
 $IPType = "IPv4"
-$NewName = "LAB-Server"
+$NewName = "LAB-SERVER"
 
 # Retrieve the network adapter that you want to configure
 $adapter = Get-NetAdapter | ? {$_.Status -eq "up"}
@@ -31,7 +31,7 @@ $adapter | New-NetIPAddress `
     -DefaultGateway $Gateway
 
 # Configure the DNS client server IP addresses
-#$adapter | Set-DnsClientServerAddress -ServerAddresses $DNS
+$adapter | Set-DnsClientServerAddress -ServerAddresses $DNS
 
-Rename-Computer -NewName $NewName
 Add-Computer -DomainName DIABETES
+Rename-Computer -NewName $NewName
