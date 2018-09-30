1883b85e5889248589b9c02c8589b810229205de
Eugene Meidinger <eugene@eugenemeidinger.com>
2018-09-30 15:01:36 -0400

    cleaned out fedore and old programs

--- master.sh
+++ master.sh
@@ -12,22 +12,10 @@ do
 	read pi
 done
 
-while [[ $debian != 'y' && $debian != 'n' ]]
-do
-	echo "Is this debian based? (y/n)"
-	read debian
-done
-
 while [[ $desktop != 'y' && $desktop != 'n' ]]
 do
 	echo "Do you want to install desktop apps? (y/n)"
 	read desktop
-done
-
-while [[ $unattended != 'y' && $unattended != 'n' ]]
-do
-	echo "Do you want to do an unattended install? (y/n)"
-	read unattended
 done
 
 
@@ -45,27 +33,19 @@ done
 	
 
 
-if [[ $debian == 'y' ]]
-then
+
 	#Update everything
 	apt-get update && apt-get -y upgrade && apt-get clean
 
 	#Install command line tools
-	apt-get -y install htop lynx irssi screen tmux aptitude mc nmap wget curl ssh vim scrot
+	apt-get -y install htop lynx screen tmux aptitude mc nmap wget curl ssh vim
 	if [[ $pi == 'y' ]]
 	then
 		apt-get -y install wicd-curses
 	fi
-
-	#Install command line games
-	apt-get -y install angband angband-doc crawl gearhead gearhead2 moria omega-rpg bsdgames freesweep gnobots2 zangband zangband-data slashem nethack-console bombardier 
-
-	#install samaba
-	apt-get -y install cifs-utils 
-	apt-get -y install samaba-common-bin
-
+ 
 	#Install command line coding tools
-	apt-get -y install git valgrind python-pip
+	apt-get -y install valgrind python-pip
 	if [[ $pi == 'y' ]]
 	then
 		apt-get -y install python-rpi.gpio
@@ -77,26 +57,15 @@ then
 	#clean again
 	apt-get clean
 
-	#install C# programming
-	apt-get -y install mono-complete monodevelop monodevelop-database monodevelop-versioncontrol
-	
+
 	if [[ $pi == 'n' ]]
 	then
-		#install haskell
-		apt-get -y install ghc cabal-install
 
 		#install arm toolchain
 		apt-get -y install gcc-arm-none-eabi
 	fi
 
-	#install ruby
-	apt-get -y install ruby ruby1.9.1-dev libsqlite3-dev ri ruby-dev
 
-	if [[ $unattended = 'n' ]]
-	then
-		#install lamp
-		apt-get -y install apache2 php5 mysql-client mysql-server phpmyadmin
-	fi    
 
 	if [[ $pi == 'y' ]] 
 	then
@@ -116,126 +85,21 @@ then
 	#clean again, just to be safe
 	apt-get clean
 
-	if [[ $desktop == 'y' ]]
+	if [[ $desktop == 'y' && $pi == 'n' ]]
 	then
-		#install desktop apps
-		apt-get -y install iceweasel geany remmina remmina-plugin-rdp evince zenmap gparted
-#		if [[ -e /etc/xdg/lxsession/LXDE-pi/autostart && ! grep -q iceweasel /etc/xdg/lxsession/LXDE-pi/autostart ]]	
-#		then
-#			echo "@iceweasel" >> /etc/xdg/lxsession/LXDE-pi/autostart
-#		fi
+
+        apt-get install anki python3-distutils
+
 		#Install VS code
 		curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-		mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-		echo "deb [arch=amd64] http://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list
-		sudo apt-get update
-		sudo apt-get install code
+        install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+        sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+        apt-get install apt-transport-https		
+        apt-get update
+		apt-get install code
 		xdg-mime default code.desktop text/plain
-	fi
-fi
-
-
-if [[ $debian == 'n' ]]
-then
-	#update everything
-	yum -y update
-
-	#install command line tools
-	yum -y install htop lynx irssi screen tmux mc nmap wget curl ssh
-	if [[ $pi == 'y' ]]
-	then
-		yum -y install wicd-curses
+        update-alternatives --set editor /usr/bin/code
 	fi
 
-	#Install command line games
-	yum -y install nethack-console bombardier
-
-	#install command line coding tools
-	yum -y install git valgrind vim python-pip
-
-	#install samba
-	yum -y install samba samba-client system-config-samba cifs-utils
-
-	#install c tools
-	yum -y groupinstall development-tools
-	yum -y install libedit-dev* man-pages libstdc++-docs
-
-	#install C# programming
-	yum -y install monodevelop monocomplete monodevelop-database monodevelop-versioncontrol
-
-	if [[ $pi == 'n' ]]
-	then
-		#install haskell
-		yum -y install ghc cabal-install
-	fi
-
-	#install ruby
-	yum -y install ruby ruby-devel sqlite3-devel
-
-	if [[ $unattended == 'n' ]]
-	then
-		#install lamp
-		yum -y install httpd mysql mysql-server php php-mysql php-myadmin
-		service httpd start; service mysql start
-		#/user/bin/mysql_secure_installation
-	fi
-
-	#install mean
-	#http://docs.mongodb.org/manual/tutorial/install-mongodb-on-red-hat/
-	yum -y install mongodb-org epel-release nodejs npm
-	service mongod start
-	
-	if [[ $desktop = 'y' ]]
-	then
-		#install desktop apps
-		yum -y install eric geany lazarus drpython remmina remmina-plugin-rdp zenmap gparted
-		
-
-		#install a bunch of random stuff, I don't know if any of this works
-		yum -y install scala erlang eclipse golang rust clojure dmd ldc gdc
-
-		if [[ $pi == 'y' ]] ||   yum -q list steam | grep -q steam
-		then
-			echo no steam for you!
-		else
-			yum -y install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-stable.noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-stable.noarch.rpm
-		
-			#install codecs 
-			yum -y install gstreamer{1,}-{ffmpeg,libav,plugins-{good,ugly,bad{,-free,-nonfree}}} ffmpeg 
-			yum -y install steam
-			yum installi gnash-plugin
-		fi
-
-		rpm --import https://packages.microsoft.com/keys/microsoft.asc
-		echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo
-		yum check-update
-		yum install code
-		xdg-mime default code.desktop text/plain
-	fi
-fi
-
-if [[ $desktop = 'y' ]]
-then
-    #install powerline
-    pip install -U pip
-    pip install git+git://github.com/Lokaltog/powerline
-fi
-
-#install gems
-gem install --no-rdoc --no-ri   sinatra haml slim rails shotgun pry bundler tmuxinator
-#install gems for fuzzyl's stuff
-gem install --no-rdoc --no-ri   git json redcarpet
-
-#install node packages
-npm -g update
-npm -g install npm
-npm -g install azure-cli express bower grunt gulp yo
-npm -g install @microsoft/sharepoint
-
-if [[ $pi == 'n' ]]
-then
-	cabal update
-	cabal install vector statistics attoparsec criterion
-fi
 
 exit 0
